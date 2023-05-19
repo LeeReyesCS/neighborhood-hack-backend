87fcbd40aec0fbc0915e6c5af482a40260dc9dd7
Farrah <farrah.unger24@gmail.com>
2023-05-19 19:38:21 -0400

    fixed typos

--- app/models/comments.py
+++ app/models/comments.py
@@ -5,5 +5,5 @@ class Comment(db.Model):
     comment_text = db.Column(db.String)
     timestamp = db.Column(db.DateTime, nullable=False) # default = datetime.now
     message = db.Column(db.string, nullable=False)
-    user_id = db.Colum(db.Integer, db.ForeignKey="user.user_id")
-    board_id = db.Column(db.Intger, db.ForeignKety("board.board_id"))
+    user_id = db.Colum(db.Integer, db.ForeignKey("user.user_id"))
+    board_id = db.Column(db.Intger, db.ForeignKey("board.board_id"))
--- app/models/users.py
+++ app/models/users.py
@@ -3,6 +3,7 @@ from app import db
 class User(db.Model):
     user_id  = db.Column(db.Integer, autoincrement = True, primary_key = True)
     name = db.Column(db.String)
+    password = db.Column(db.String)
     zipcode = db.Column(db.Integer)
     email = db.Column(db.String)
     phone = db.Column(db.String)
